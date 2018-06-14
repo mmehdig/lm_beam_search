20466805fe16485faafa2a0c987ca965ea43cd11
Mehdi <mmehdi.g@gmail.com>
2018-06-14 15:54:01 +0200

    Update beam_search.py

--- beam_search.py
+++ beam_search.py
@@ -10,7 +10,7 @@ def search(model, src_input, k=1, sequence_max_len=25):
         for prob, sent_predict in k_beam:
             predicted = model.predict([np.array([src_input]), np.array([sent_predict])])[0]
             # top k!
-            possible_k_trgs = predicted[l].argsort()[-k:][::-1]
+            possible_k = predicted[l].argsort()[-k:][::-1]
 
             # add to all possible candidates for k-beams
             all_k_beams += [
@@ -18,7 +18,7 @@ def search(model, src_input, k=1, sequence_max_len=25):
                     sum(np.log(predicted[i][sent_predict[i+1]]) for i in range(l)) + np.log(predicted[l][next_wid]),
                     list(sent_predict[:l+1])+[next_wid]+[0]*(sequence_max_len-l-1)
                 )
-                for next_wid in possible_k_trgs
+                for next_wid in possible_k
             ]
 
         # top k
