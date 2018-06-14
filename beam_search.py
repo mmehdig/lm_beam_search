de9c775a39b000a8de383a4623e1f76091369dc1
Mehdi <mmehdi.g@gmail.com>
2018-06-14 15:36:49 +0200

    Update beam_search.py

--- beam_search.py
+++ beam_search.py
@@ -5,16 +5,16 @@ def search(model, src_input, k=1, sequence_max_len=25):
     # l : point on target sentence to predict
     for l in range(sequence_max_len):
         all_k_beams = []
-        for prob, trg_sent_predict in k_beam:
-            predicted = model.predict([np.array([src_input]), np.array([trg_sent_predict])])[0]
+        for prob, sent_predict in k_beam:
+            predicted = model.predict([np.array([src_input]), np.array([sent_predict])])[0]
             # top k!
             possible_k_trgs = predicted[l].argsort()[-k:][::-1]
 
             # add to all possible candidates for k-beams
             all_k_beams += [
                 (
-                    sum(np.log(predicted[i][trg_sent_predict[i+1]]) for i in range(l)) + np.log(predicted[l][next_wid]),
-                    list(trg_sent_predict[:l+1])+[next_wid]+[0]*(sequence_max_len-l-1)
+                    sum(np.log(predicted[i][sent_predict[i+1]]) for i in range(l)) + np.log(predicted[l][next_wid]),
+                    list(sent_predict[:l+1])+[next_wid]+[0]*(sequence_max_len-l-1)
                 )
                 for next_wid in possible_k_trgs
             ]
